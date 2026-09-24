840f07e8a15d7fa4bd6add747a82cf46bee05a6e
4IK25CS217 <2025cs_surajsp_g@nie.ac.in>
2026-09-24 18:02:14 +0530

    list compresion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-evens = []
-for num in numbers:
-    if num % 2 == 0:
-        evens.append(num)
+evens = [num for num in numbers if num % 2 == 0]
 
 print(evens)  # Output: [2, 4, 6, 8, 10]
